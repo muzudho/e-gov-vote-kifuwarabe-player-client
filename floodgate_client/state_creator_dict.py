5a3fb4ac302cf37b7d4e5d0234bea0a380cb46e1
むずでょ <muzudho1@gmail.com>
2021-12-02 20:59:13 +0900

    create listen

--- floodgate_client/state_creator_dict.py
+++ floodgate_client/state_creator_dict.py
@@ -1,6 +1,7 @@
 from floodgate_client.layer2_decoration_event.init import create as create_init
 from floodgate_client.layer2_decoration_event.entrance import create as create_entrance
 from floodgate_client.layer2_decoration_event.lobby import create as create_lobby
+from floodgate_client.layer2_decoration_event.listen import create as create_listen
 from floodgate_client.layer2_decoration_event.reply import create as create_reply
 from floodgate_client.layer2_decoration_event.play import create as create_play
 
@@ -8,6 +9,7 @@ state_creator_dict = {
     "[Init]": create_init,  # 初期値
     "[Entrance]": create_entrance,  # 初期値
     "[Lobby]": create_lobby,
+    "[Listen]": create_listen,
     "[Reply]": create_reply,
     "[Play]": create_play
 }
